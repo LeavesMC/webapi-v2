013536902da26b4949c10ea5ecbe2bba4ef6da99
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-07-28 22:44:01 +0800

    添加.gitignore

---
+++ src/app/routers/index.ts
@@ -0,0 +1,7 @@
+import router from "../router";
+import { readFileSync } from "fs";
+
+router.on("/", async function(data, response) {
+    response.contentType = "text/html";
+    response.response = readFileSync(process.cwd() + "/public/index.html");
+})
