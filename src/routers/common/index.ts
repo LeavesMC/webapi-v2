277795b525397c0465043b68872e1e2edcc6bcf5
bluemangoo <chenfy2006@qq.com>
2023-07-31 09:36:30 +0800

    Feat: redirect index to docs

--- src/routers/common/index.ts
+++ src/routers/common/index.ts
@@ -1,5 +1,7 @@
 import router from "../router";
 
 router.on("/", async function(_data, response) {
-    response.response = "LeavesMC API";
+    response.redirect = true;
+    response.status = 302;
+    response.redirectUrl = "/v2/docs";
 });
