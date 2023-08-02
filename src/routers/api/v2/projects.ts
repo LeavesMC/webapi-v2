d2f0a736e18c93e82963d59c7028bc56ae519a83
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-08-02 10:25:17 +0800

    修复/v2/projects API

--- src/routers/api/v2/projects.ts
+++ src/routers/api/v2/projects.ts
@@ -1,30 +1,20 @@
 import router from "../../router";
 import mongo from "../../../utils/mongo";
 import restError from "../../../utils/restError";
+import PROJECTS from "../../../utils/projects";
 
 router.on("/v2/projects", async function(request, response) {
     response.contentType = "application/json";
-    try {
-        const client = mongo.client;
-        await client.connect();
-        const record = client
-            .db("metadata")
-            .collection("projects")
-            .find({});
-        const result: any[] = [];
-        for await (const doc of record) {
-            result.push({
-                id: doc.id,
-                name: doc.name
-            });
-        }
-        response.response = {
-            code: 200,
-            data: result
-        };
-        await client.close();
-    } catch (e) {
-        console.error(e);
-        return restError.$500(response);
-    }
+    const result: any[] = [];
+    PROJECTS.forEach((value,key)=>{
+        result.push({
+            id: key,
+            name: value.name,
+            repo: value.repo
+        });
+    })
+    response.response = {
+        code: 200,
+        data: result
+    };
 });
