4e0cebc97d4c2bd5c82917825f5cde73e9d7aeb3
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-08-03 20:40:39 +0800

    更新download api

---
+++ src/routers/api/v2/projects/versions/builds/downloads/download.ts
@@ -0,0 +1,42 @@
+import router from "../../../../../../router";
+import mongo from "../../../../../../../utils/mongo";
+import PROJECTS from "../../../../../../../utils/projects";
+import restError from "../../../../../../../utils/restError";
+import Utils from "../../../../../../../utils/utils";
+
+router.pattern(/^\/v2\/projects\/\S+\/versions\/\S+\/builds\/\S+\/downloads\/\S+$/, async function (request, response) {
+    try {
+        const projectId = request.url.split("/")[3];
+        if (!PROJECTS.has(projectId)) {
+            return restError.$404(response);
+        }
+        const projectData = PROJECTS.get(projectId);
+        const version = request.url.split("/")[5];
+        const build = parseInt(request.url.split("/")[7]);
+        const download = request.url.split("/")[9];
+        const client = mongo.client;
+        await client.connect();
+        const dbResult = (await client
+            .db(projectId)
+            .collection(Utils.getVersionGroup(version))
+            .find({
+                version: {
+                    $eq: version
+                },
+                build_id: {
+                    $eq: build
+                }
+            }).toArray())[0];
+        let downloadUrl = `https://github.com/${PROJECTS.get(projectId).repo}/releases/download/${version}-${dbResult.tag}/${projectId}-${version}.jar`;
+        if (download === "ghproxy") {
+            downloadUrl = `https://ghproxy.com/?q=${encodeURIComponent(downloadUrl)}`
+        }
+        response.status = 301;
+        response.redirect = true;
+        response.contentType = "application/java-archive";
+        response.redirectUrl = downloadUrl;
+    } catch (e) {
+        console.error(e);
+        return restError.$500(response);
+    }
+});
