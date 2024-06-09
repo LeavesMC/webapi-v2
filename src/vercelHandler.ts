f7490f07ff48a88f816558979af2b48d317f6771
Bluemangoo <chenfy2006@qq.com>
2024-06-09 21:25:26 +0800

    fix

--- src/vercelHandler.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "./app";
-
-export default async function(request: VercelRequest, response: VercelResponse) {
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    await app.vercelHandler()(request, response);
-};
