4f328364a786d1d85ab860a8d2cf71c56062f8ec
Bluemangoo <chenfy2006@qq.com>
2024-06-29 17:11:30 +0800

    feat: add back root redirect

--- src/routers/common/index.ts
+++ src/routers/common/index.ts
@@ -1,7 +1,6 @@
 import router from "../router";
 
 router.on("/", async function(_data, response) {
-    // response.redirect = true;
-    // response.status = 302;
-    // response.redirectUrl = "/v2/docs";
+    response.redirect = true;
+    response.redirectUrl = "/v2/docs";
 });
