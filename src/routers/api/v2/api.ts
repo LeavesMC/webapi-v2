7d0b9b6643b069988182f6d16a3026a1eab284ae
Bluemangoo <chenfy2006@qq.com>
2024-07-03 08:35:44 +0800

    fix: read file error
    小黑！！！

--- src/routers/api/v2/api.ts
+++ src/routers/api/v2/api.ts
@@ -3,5 +3,5 @@ import { readFileSync } from "fs";
 
 router.on("/v2/api", async function(request, response) {
     response.contentType = "application/json";
-    response.response = readFileSync(process.cwd() + "/public/API-V2.json");
+    response.response = readFileSync(process.cwd() + "/public/API-V2.json","utf8");
 })
