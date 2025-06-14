d650366a726ace9bc3274a51625095ffa8224f08
LittleChest <hiya@littlew.top>
2025-06-14 22:56:23 +0800

    feat: Add webhook support

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,6 +2,8 @@ class Env{
     public static readonly MONGODB_URI:string = process.env["MONGODB_URI"];
     public static readonly API_PUBLIC_KEY:string = process.env["API_PUBLIC_KEY"];
     public static readonly API_PRIVATE_KEY:string = process.env["API_PRIVATE_KEY"];
+    public static readonly WEBHOOK_PASSWORD:string = process.env["WEBHOOK_PASSWORD"];
+    public static readonly WEBHOOK_ENTRYPOINT:string = process.env["WEBHOOK_ENTRYPOINT"];
 }
 
 export default Env;
---
+++ src/utils/webhook.ts
@@ -0,0 +1,53 @@
+import { createHash } from "crypto";
+import Env from "./env";
+
+export default async function triggerWebhook(
+    repo: string,
+    project: string,
+    version: string,
+    build?: number,
+    commit?: string,
+    tag?: string
+): Promise<Response> {
+    try {
+        const payload = {
+            repo: repo,
+            project: project,
+            version: version,
+            build: build,
+            commit: commit,
+            tag: tag
+        };
+        const authHeader = createHash("md5")
+            .update(
+                parseInt(String(Math.floor(Date.now() / 1000)).slice(0, -1), 10) +
+                    Env.WEBHOOK_PASSWORD
+            )
+            .digest("hex");
+        const response = await fetch(Env.WEBHOOK_ENTRYPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-LeavesMC-Authorization": authHeader
+            },
+            body: JSON.stringify(payload)
+        });
+        if (response.status !== 202) {
+            if (response.status === 403) {
+                await sleep(3000);
+                triggerWebhook(repo, project, version, build, commit, tag); // Retry
+            }
+            if (response.status === 500) {
+                await sleep(5000);
+                triggerWebhook(repo, project, version, build, commit, tag); // Retry
+            }
+            return;
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
