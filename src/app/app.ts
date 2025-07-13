e10cdfb331ffdde37ea98b49ee7c1de2b423e3ac
MC_XiaoHei <xor7xiaohei@gmail.com>
2025-07-13 09:43:51 +0800

    feat: add cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
-import VCLight from "vclight";
+import VCLight, { VCLightRequest, VCLightResponse } from "vclight";
 import router from "./router";
 import "./initRouter";
 import "./utils/env";
 import { initDb } from "./utils/db/db";
 
 const app = new VCLight();
+app.use({
+    async process(_: VCLightRequest, __: VCLightResponse, ___: VCLight): Promise<void> {
+    },
+    async post(_: VCLightRequest, response: VCLightResponse, ___: VCLight): Promise<void> {
+        response.headers["access-control-allow-origin"] = "*";
+    }
+});
 
 initDb().then(() => {
     app.use(router);
